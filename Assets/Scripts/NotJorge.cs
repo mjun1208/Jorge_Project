999acd672e886db03ad2f6ea2f72a7612e2a41a0
mjun1208 <37032675+mjun1208@users.noreply.github.com>
2019-05-19 18:22:58 +0900

    복제 조지 따라오게 만듬

---
+++ Assets/Scripts/NotJorge.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NotJorge : MonoBehaviour
+{
+
+    private GameObject Player;
+    private NavMeshAgent nav;
+    private Animator anime; 
+    //private Transform Pos;  
+
+    //NavMeshAgent Ang;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        nav = GetComponent<NavMeshAgent>();
+        anime = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       nav.SetDestination(Player.transform.position);
+       AnimeUpdate();
+    }
+
+    void AnimeUpdate()
+    {
+        if (nav.destination != transform.position)
+            anime.SetBool("Move", true);
+        else
+            anime.SetBool("Move", false);
+    }
+}
