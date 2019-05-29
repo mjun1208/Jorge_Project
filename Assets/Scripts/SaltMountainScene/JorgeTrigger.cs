1594326b67d991b362b1ba42e1ba49f0fa96002e
BangJeongHyeok <46840940+BangJeongHyeok@users.noreply.github.com>
2019-05-29 11:29:38 +0900

    신규전장 비켄디 제작

---
+++ Assets/Scripts/SaltMountainScene/JorgeTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JorgeTrigger : MonoBehaviour
+{
+    public GameObject NotJorge;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        GameObject.Destroy(NotJorge);
+        GameObject.Destroy(gameObject);
+    }
+}
